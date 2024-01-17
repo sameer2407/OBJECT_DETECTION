9bc716a3f6300b9d42dad01a8335c54aaca4ca16
SAMEER <93471294+sameer2407@users.noreply.github.com>
2024-01-17 10:46:15 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -15,16 +15,16 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
         enable_edgetpu: bool) -> None:
 
 
-  # Variables to calculate FPS
+  
   counter, fps = 0, 0
   start_time = time.time()
 
-  # Start capturing video input from the camera
+  
   cap = cv2.VideoCapture(camera_id)
   cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
   cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-  # Visualization parameters
+
   row_size = 20  # pixels
   left_margin = 24  # pixels
   text_color = (0, 0, 255)  # red
@@ -32,7 +32,7 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
   font_thickness = 1
   fps_avg_frame_count = 10
 
-  # Initialize the object detection model
+  
   base_options = core.BaseOptions(
       file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
   detection_options = processor.DetectionOptions(
@@ -41,7 +41,7 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
       base_options=base_options, detection_options=detection_options)
   detector = vision.ObjectDetector.create_from_options(options)
 
-  # Continuously capture images from the camera and run inference
+  
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -55,13 +55,13 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
     # Convert the image from BGR to RGB as required by the TFLite model.
     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Create a TensorImage object from the RGB image.
+    
     input_tensor = vision.TensorImage.create_from_array(rgb_image)
 
-    # Run object detection estimation using the model.
+   
     detection_result = detector.detect(input_tensor)
 
-    # Draw keypoints and edges on input image
+    
     image = utils.visualize(image, detection_result)
 
     # Calculate the FPS
